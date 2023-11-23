69b82f1e479da2bad854c945c7cd3a6c6ac0bafd
Tejas Ladani <77571495+tejasladani999@users.noreply.github.com>
2023-11-23 14:46:28 +0530

    draw the scatter chart of our data.

---
+++ scatter+line.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  7 16:39:53 2022
+
+@author: KAMAL
+"""
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5,6]
+y = [5,10,15,20,25,30]
+
+plt.scatter(x,y)
+plt.plot(x,y)
+plt.show()
